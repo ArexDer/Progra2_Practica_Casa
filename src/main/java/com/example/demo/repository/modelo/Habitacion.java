10ef5ecea30cde3463375a56628b4b9d8a6a168b
Diego Rivas <darivas@uce.edu.ec>
2023-06-19 02:23:55 -0500

    Main de mis repasos en casa

---
+++ src/main/java/com/example/demo/repository/modelo/Habitacion.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository.modelo;
+
+import java.math.BigDecimal;
+
+import java.math.BigDecimal;
+
+public class Habitacion {
+	
+	private Integer id;
+
+	//atributos logicos que el usuario conoce
+	private String numero;
+	private BigDecimal valor;
+
+}
---
+++ src/main/java/com/example/demo/repository/modelo/Hotel.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository.modelo;
+
+public class Hotel {
+	
+	private Integer id;
+	private String nombre;
+	private String direccion;
+
+}
